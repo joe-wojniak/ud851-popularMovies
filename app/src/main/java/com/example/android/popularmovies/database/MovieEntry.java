f281b85c607b31beaa15572d33c9fa89ae101f82
Joe Wojniak <joe.wojniak@gmail.com>
2018-12-27 16:19:16 -0700

    added Room database and Executors to initiate threads (not functional)

---
+++ app/src/main/java/com/example/android/popularmovies/database/MovieEntry.java
@@ -0,0 +1,107 @@
+package com.example.android.popularmovies.database;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "movie")
+public class MovieEntry {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String release_date;
+    private String posterPath;
+    private String vote_average;
+    private String overview;
+    private String popularity;
+    private String isFavorite;
+
+    @Ignore
+    public MovieEntry(String title, String release_date, String posterPath, String vote_average,
+                      String overview, String popularity, String isFavorite) {
+        this.title = title;
+        this.release_date = release_date;
+        this.posterPath = posterPath;
+        this.vote_average = vote_average;
+        this.overview = overview;
+        this.popularity = popularity;
+        this.isFavorite = isFavorite;
+    }
+
+    public MovieEntry(int id, String title, String release_date, String posterPath, String vote_average,
+                      String overview, String popularity, String isFavorite) {
+        this.id = id;
+        this.title = title;
+        this.release_date = release_date;
+        this.posterPath = posterPath;
+        this.vote_average = vote_average;
+        this.overview = overview;
+        this.popularity = popularity;
+        this.isFavorite = isFavorite;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(String vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getIsFavorite() {
+        return isFavorite;
+    }
+
+    public void setIsFavorite(String isFavorite) {
+        this.isFavorite = isFavorite;
+    }
+}
